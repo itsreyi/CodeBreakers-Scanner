e499853ab63c3a637ac5d75157a16ae9347468f5
itsreyi <alessandro.pati@edu.unife.it>
2024-04-13 19:23:23 +0200

    v 1.0.0

--- CB_Scanner.py
+++ CB_Scanner.py
@@ -19,7 +19,6 @@ def get_valid_input(prompt, options):
             print('')
 
 def strip_color(text):
-    # Regular expression to match ANSI escape codes for colors
     ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
     return ansi_escape.sub('', text)
 
